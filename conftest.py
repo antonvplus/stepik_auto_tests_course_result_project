df60bc440355d1cf3fec08fde3f675b40fe82394
Anton Maslov <anton.maslov@ringcentral.com>
2026-05-25 16:33:01 +0300

    Start file for result project

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help="Selecting the website language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    chrome_options = Options()
+    chrome_options.add_experimental_option("prefs", {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=chrome_options)
+    yield browser
+    browser.quit()
